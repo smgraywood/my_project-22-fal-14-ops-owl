5134e017304f50ecdd554e051f75c15e9fa66456
boscochw_ubuntU_ideapad <bosco.chw@gmail.com>
2022-09-20 22:17:24 -0700

    display portfolio dynamically with json file

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask, render_template, request
 from dotenv import load_dotenv
+import json
 
 load_dotenv()
 app = Flask(__name__)
@@ -8,4 +9,25 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', title="The Ops Owls", url=os.getenv("URL"))
+    data = getData()
+    names = map(lambda x: x["name"].lower(), data)
+    return render_template('index.html', title="The Ops Owls", url=os.getenv("URL"), names=names)
+
+@app.route('/profile/<string:name>')
+def portfolio(name):
+    data = getData()
+
+    portfolio_data = None
+    for person in data:
+        if person["name"].lower() == name.lower():
+            portfolio_data = person
+
+    return render_template("profile.html", portfolio_data=portfolio_data)
+
+
+def getData():
+    SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
+    data_file = open(os.path.join(SITE_ROOT, "data.json"))
+    data = json.load(data_file)
+
+    return data
