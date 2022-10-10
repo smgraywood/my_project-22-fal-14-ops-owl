45932e24234ab991645c546af972b084d6313917
Sarah Graywood <sarahgraywood@Sarahs-MacBook-Pro.local>
2022-10-10 15:59:02 -0700

    adding curl-test

--- curl-test.sh
+++ curl-test.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-curl --request GET http://localhost:5000/api/timeline_post
-{"timeline_posts": []}
+curl --request POST http://localhost:5000/api/timeline_post -d 'name=Sarah&email=smgraywood@gmail.com&content=Just Added Database to Portfolio Site!' {"content":"Just Added Database to Portfolio Site!","created_at":"Mon, 10 Oct 2022 08:50:30 GMT","email":"smgraywood@gmail.com", "id":1,"name":"Sarah"}
 
-curl http://localhost:5000/api/timeline_post
-{"timeline_posts": []}
+curl -X POST http://localhost:5000/api/timeline_post -d 'name=Sarah&email=smgraywood@gmail.com&content=Testing endpoints with postman and curl' {"content":"Testing endpoints with postman and curl","created_at":"Mon, 10 Oct 2022 08:50:30 GMT","email":"smgraywood@gmail.com", "id":1,"name":"Sarah"}
+
+
+curl --request GET http://localhost:5000/api/timeline_post {"timeline_posts": []}
+
+curl http://localhost:5000/api/timeline_post {"timeline_posts": []}
