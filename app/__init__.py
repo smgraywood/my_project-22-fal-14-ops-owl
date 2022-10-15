f45aba4a75a49f767d4fd40ee1afafe203f2d6fd
Sarah Graywood <sarahgraywood@Sarahs-MacBook-Pro.local>
2022-10-14 17:22:48 -0700

    updates to init file and timeline

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 from peewee import *
 import json
 import datetime
+import re
 from playhouse.shortcuts import model_to_dict
 
 load_dotenv()
@@ -31,25 +32,48 @@ class TimelinePost(Model):
 mydb.connect()
 mydb.create_tables([TimelinePost])
 
+print(mydb)
 
 @app.route('/api/timeline_post', methods=['POST'])
 def post_time_line_post():
-    name = request.form['name']
-    email = request.form['email']
-    content = request.form['content']
+    try:
+        name = request.form['name']
+    except Exception as e:
+        return "Invalid name", 400
+    else:
+        if name == '':
+            return "Invalid name", 400
+    
+    try:
+       email = request.form['email']
+    except Exception as e:
+        return "Invalid email", 400
+    else:
+        if email == '':
+            return "Invalid email", 400
+
+    try:
+        content = request.form['content']
+    except Exception as e:
+        return "Invalid content", 400
+    else:
+        if content == '':
+            return "Invalid content", 400
+
+    if not re.match(r"^[A-Za-z0-9\.\+_-]+@[A-Za-z0-9\._-]+\.[a-zA-Z]*$", email):
+        return "Invalid email", 400
+    
     timeline_post = TimelinePost.create(name=name, email=email, content=content)
     return model_to_dict(timeline_post)
 
-@app.route('/api/timeline_post', methods=['GET'])
+@app.route("/api/timeline_post", methods=['GET'])
 def get_time_line_post():
     return {
         'timeline_posts': [
             model_to_dict(p)
-            for p in
-TimelinePost.select().order_by(TimelinePost.created_at.desc())
+            for p in TimelinePost.select().order_by(TimelinePost.created_at.desc())
         ]
     }
-
 
 @app.route('/')
 def index():
@@ -72,6 +96,9 @@ def portfolio(name):
 
     return render_template("profile.html", portfolio_data=portfolio_data)
 
+@app.route('/timeline')
+def timeline():
+    return render_template('timeline.html', title="Timeline")
 
 def getData():
     SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
@@ -79,3 +106,4 @@ def getData():
     data = json.load(data_file)
 
     return data
+
