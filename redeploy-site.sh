c2cdfa682b9d566edc7862827a9e0938ce140150
Sarah Graywood <sarahgraywood@Sarahs-MacBook-Pro.local>
2022-10-15 12:31:15 -0700

    updates to redeploy script

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-
-tmux kill-server
-
-tmux new-session -d -s project_session
-
 cd ./app
 
 git fetch && git reset origin/main --hard
@@ -14,6 +9,11 @@ source python3-virtualenv/bin/activate
 
 pip install -r requirements.txt
 
+#fails at running db (mydb.connect)
 export FLASK_RUN_HOST=0.0.0.0
 
 flask run --host=FLASK_RUN_HOST
+
+systemctl start myportfolio
+
+systemctl enable myportfolio
