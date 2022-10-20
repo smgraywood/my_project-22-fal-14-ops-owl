87effa837bb6df306a84b03ab0a4a8b833018da1
Sarah Graywood <sarahgraywood@Sarahs-MacBook-Pro.local>
2022-10-20 13:17:02 -0700

    fixes to database and form

--- app/__init__.py
+++ app/__init__.py
@@ -1,37 +1,35 @@
 import os
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 from dotenv import load_dotenv
 from peewee import *
 import json
 import datetime
 import re
 from playhouse.shortcuts import model_to_dict
-import re
 
 load_dotenv()
 app = Flask(__name__)
 
-# if os.getenv("TESTING") == "true":
-#     print("Running in test mode")
-#     mydb =SqliteDatabase('file:memory?mode=memory&cache=shared',
-# uri=True)
-# else:
-#     mydb = MySQLDatabase(os.getenv("MYSQL_DATABASE"),
-#     user=os.getenv("MYSQL_USER"),
-#     password=os.getenv("MYSQL_PASSWORD"),
-#     host=os.getenv("MYSQL_HOST"),
-#     port=3306
-# )
-
-mydb = MySQLDatabase(os.getenv("MYSQL_DATABASE"),
+if os.getenv("TESTING") == "true":
+    print("Running in test mode")
+    mydb =SqliteDatabase('file:memory?mode=memory&cache=shared',
+uri=True)
+else:
+    mydb = MySQLDatabase(os.getenv("MYSQL_DATABASE"),
     user=os.getenv("MYSQL_USER"),
     password=os.getenv("MYSQL_PASSWORD"),
     host=os.getenv("MYSQL_HOST"),
     port=3306
 )
 
+# mydb = MySQLDatabase(os.getenv("MYSQL_DATABASE"),
+#     user=os.getenv("MYSQL_USER"),
+#     password=os.getenv("MYSQL_PASSWORD"),
+#     host=os.getenv("MYSQL_HOST"),
+#     port=3306
+# )
+
 print(mydb)
-...
 
 class TimelinePost(Model):
     name = CharField()
@@ -47,43 +45,24 @@ mydb.create_tables([TimelinePost])
 
 print(mydb)
 
-# @app.route('/api/timeline_post', methods=['POST'])
-# def post_time_line_post():
-#     name = request.form['name']
-#     email = request.form['email']
-#     content = request.form['content']
-#     timeline_post = TimelinePost.create(name=name, email=email,content=content)
-#     return model_to_dict(timeline_post)
+
 
 @app.route('/api/timeline_post', methods=['POST'])
 def post_time_line_post():
-    try:
-        name = request.form['name']
-    except Exception as e:
-        return "Invalid name", 400
-    else:
-        if name == '':
-            return "Invalid name", 400
-    
-    try:
-       email = request.form['email']
-    except Exception as e:
-        return "Invalid email", 400
-    else:
-        if email == '':
-            return "Invalid email", 400
+    # name =request.form['name']
+    # email= request.form['email']
+    # content=request.form['content']
+    name = request.form.get('name')
+    email = request.form.get('email', '')
+    content = request.form.get('content')
+    print(email)
+    print(name)
+    print(content)
+    if not all([name, re.match(r"^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$", email), content]):
+        return jsonify("Invalid input"), 400
 
-    try:
-        content = request.form['content']
-    except Exception as e:
-        return "Invalid content", 400
-    else:
-        if content == '':
-            return "Invalid content", 400
+# r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$"
 
-    if not re.match(r"^[A-Za-z0-9\.\+_-]+@[A-Za-z0-9\._-]+\.[a-zA-Z]*$", email):
-        return "Invalid email", 400
-    
     timeline_post = TimelinePost.create(name=name, email=email, content=content)
     return model_to_dict(timeline_post)
 
---
+++ app/oldness.py
@@ -0,0 +1,31 @@
+@app.route('/api/timeline_post', methods=['POST'])
+def post_time_line_post():
+    try:
+        name = request.form['name']
+    except Exception as e:
+        return "Invalid name", 400
+    else:
+        if name == '':
+            return "Invalid name", 400
+    
+    try:
+        email = request.form['email']
+    except Exception as e:
+        return "Invalid email", 400
+    else:
+        if email == '':
+            return "Invalid email", 400
+
+    try:
+        content = request.form['content']
+    except Exception as e:
+        return "Invalid content", 400
+    else:
+        if content == '':
+            return "Invalid content", 400
+
+    if not re.match(r"^[A-Za-z0-9\.\+_-]+@[A-Za-z0-9\._-]+\.[a-zA-Z]*$", email):
+        return "Invalid email", 400
+    
+    timeline_post = TimelinePost.create(name=name, email=email, content=content)
+    return model_to_dict(timeline_post)
