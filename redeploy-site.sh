79420fc4293d0c5c5596f4353ec29891897a08a0
Sarah Graywood <sarahgraywood@Sarahs-MacBook-Pro.local>
2022-10-20 13:42:35 -0700

    changes to redeploy script

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,19 +1,10 @@
 #!/bin/bash
-cd ./app
+cd ~/my_project-22-fal-14-ops-owl
 
 git fetch && git reset origin/main --hard
-
-python3 -m venv python3-virtualenv
 
 source python3-virtualenv/bin/activate
 
 pip install -r requirements.txt
 
-#fails at running db (mydb.connect)
-export FLASK_RUN_HOST=0.0.0.0
-
-flask run --host=FLASK_RUN_HOST
-
-systemctl start myportfolio
-
-systemctl enable myportfolio
+systemctl restart myportfolio
